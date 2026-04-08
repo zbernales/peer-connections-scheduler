330cab2373846821ef5f7fbe29a6ab3b733412af
zbernales <zbernn@gmail.com>
2026-04-08 14:31:10 -0700

    Created environment variable file

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -8,13 +8,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIu9PM1tX1hOHriZJ89D11XHl4jl5CLRc",
-  authDomain: "peer-connections-scheduler.firebaseapp.com",
-  projectId: "peer-connections-scheduler",
-  storageBucket: "peer-connections-scheduler.firebasestorage.app",
-  messagingSenderId: "753554740314",
-  appId: "1:753554740314:web:fad328c11c190f3b4c7463",
-  measurementId: "G-FE90NH3D30"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase

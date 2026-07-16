1dda89b675f35c4657e3e65eaa17386b7ed7fa67
zbernales <zbernn@gmail.com>
2026-07-16 13:55:40 -0700

    modern icon for export button

--- src/App.tsx
+++ src/App.tsx
@@ -1002,7 +1002,12 @@ function App() {
                           gap: '0.4rem'
                         }}
                       >
-                        📤 Export ▾
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                        <polyline points="7 10 12 15 17 10"></polyline>
+                        <line x1="12" y1="15" x2="12" y2="3"></line>
+                      </svg>
+                        Export ▾
                       </button>
 
                       {showExportModal && (
